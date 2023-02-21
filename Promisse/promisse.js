61b85ee76728990df51cf85b9f2cbde548667ba2
Raphael Busquet <raphaelbusquet@outlook.com>
2023-02-21 21:44:04 +0000

    add promises

--- Promisse/promisse.js
+++ Promisse/promisse.js
@@ -1,8 +1,13 @@
 console.log("-------Pedir Uber-------");
 
 const promessa = new Promise((resolve, reject) => {
-  return resolve("O carro chegou!");
+  return resolve("O carro chegou!"); //reject('O carro não chegou!') caso queira ver um erro.;
+
 });
 
 console.log("Aguardando...");
-promessa.then((result) => console.log(result));
+promessa
+.then((result) => console.log(result))
+.catch(erro => console.log(erro))
+.finally(() => console.log("Finalizado"));
+
